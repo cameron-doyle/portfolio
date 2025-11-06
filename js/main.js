3d9016036f00145e89eee34c878e193adfc46c3c
Cameron Doyle <dev.camerondoyle@gmail.com>
2025-11-06 23:52:52 +1100

    Added another background layer for navbar when scrolled down. Added footer.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
 var navOpen = false;
 
 $(function () {
-	console.log("Hello world")
-
 	$("#navButton").on("click", () => {
 		navOpen = !navOpen;
 
 		if (navOpen) {
-			$("#mainNav").addClass("open");
-			$("#navButton").attr('aria-expanded', String(navOpen));
+			$("#mainNav").addClass("open")
+			$("#navButton").attr('aria-expanded', String(navOpen))
 		}
 		else {
-			$("#mainNav").removeClass("open");
-			$("#navButton").attr('aria-expanded', String(navOpen));
+			$("#mainNav").removeClass("open")
+			$("#navButton").attr('aria-expanded', String(navOpen))
 		}
 	})
 
 	SetupTitleObserver()
+
+	const footerYear = $("#currentYear")
+	if (footerYear) {
+		footerYear.text(new Date().getFullYear());
+	}
 })
 
 
 function SetupTitleObserver() {
-	const title = document.querySelector('.title');
+	const title = document.querySelector('#title');
 
 	window.addEventListener('scroll', () => {
 		const rect = title.getBoundingClientRect();
 
 		if (rect.top <= 0) {
 			title.classList.add('stuck');
+			$("#navInterceptor2").addClass("open")
 		} else {
 			title.classList.remove('stuck');
+			$("#navInterceptor2").removeClass("open")
 		}
 	});
 }
